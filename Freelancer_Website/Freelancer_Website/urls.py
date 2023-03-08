d31e2baa6293fa8d4c37bf0ea31a502900aa574d
sreedhar-sk <68653217+sreedhar-sk@users.noreply.github.com>
2023-03-08 01:00:05 -0500

    Update urls.py

--- Freelancer_Website/Freelancer_Website/urls.py
+++ Freelancer_Website/Freelancer_Website/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
-from django.conf.urls import url
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("",include("django.contrib.auth.urls")),
     path("",include("allauth.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
